0ac3748fdba7d834db81d6618277a89624e2ebe7
Cruz <Onel.Cruz@G3TI.Local>
2024-04-22 00:56:47 -0400

    initial commit

---
+++ namaste/namaste_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NamasteAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'namaste_app'
---
+++ namaste/namaste_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about-us.html')
+
+def contact(request):
+    return render(request, 'contact-us.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def service(request):
+    return render(request, 'service-single.html')
