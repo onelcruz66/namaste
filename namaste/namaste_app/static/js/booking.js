9befa9d9b7ca0d85403d8cea3c4b13e1b2f00172
Cruz <Onel.Cruz@G3TI.Local>
2024-10-21 00:42:30 -0400

    Fixing bug in js booking related to the vacumtherapy name change

--- namaste/namaste_app/static/js/booking.js
+++ namaste/namaste_app/static/js/booking.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
         // Find the h3 text related to the clicked button using the class
         var packageTitle = $(this).closest('.package').find('.package-title').text();
 
+        // Check if the packageTitle is 'Maquina de Gluteos'
+        if(packageTitle === 'Maquina de Gluteos') {
+            packageTitle = 'Vacumterapia'; // Change the value to 'Vacumterapia'
+        }
+
         // Set the text in the modal's input field
         $('#selected-package').val(packageTitle);
     });
